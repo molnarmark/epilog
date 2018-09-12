972eaca5600978823516c1ecc1346fd1fc34eaff
Mark Molnar <molnarpw@gmail.com>
2018-09-13 00:51:59 +0200

    v1.1.0 - crayon dependency removed

--- src/epilog/log_types.cr
+++ src/epilog/log_types.cr
@@ -2,53 +2,53 @@ module Epilog::LogTypes
   @options = {
     "success" => {
       "emoji"      => "✔",
-      "foreground" => "light_green",
-      "background" => "black",
+      "foreground" => :light_green,
+      "background" => :black,
     },
     "error" => {
       "emoji"      => "✖",
-      "foreground" => "red",
-      "background" => "black",
+      "foreground" => :red,
+      "background" => :black,
     },
     "fatal" => {
       "emoji"      => "✖",
-      "foreground" => "red",
-      "background" => "black",
+      "foreground" => :red,
+      "background" => :black,
     },
     "warn" => {
       "emoji"      => "⚠",
-      "foreground" => "yellow",
-      "background" => "black",
+      "foreground" => :yellow,
+      "background" => :black,
     },
     "log" => {
       "emoji"      => "ℹ",
-      "foreground" => "white",
-      "background" => "black",
+      "foreground" => :white,
+      "background" => :black,
     },
     "info" => {
       "emoji"      => "ℹ",
-      "foreground" => "blue",
-      "background" => "black",
+      "foreground" => :blue,
+      "background" => :black,
     },
     "start" => {
       "emoji"      => "●",
-      "foreground" => "blue",
-      "background" => "black",
+      "foreground" => :blue,
+      "background" => :black,
     },
     "ready" => {
       "emoji"      => "♥",
-      "foreground" => "green",
-      "background" => "black",
+      "foreground" => :green,
+      "background" => :black,
     },
     "debug" => {
       "emoji"      => "…",
-      "foreground" => "dark_gray",
-      "background" => "black",
+      "foreground" => :dark_gray,
+      "background" => :black,
     },
     "trace" => {
       "emoji"      => "…",
-      "foreground" => "dark_gray",
-      "background" => "black",
+      "foreground" => :dark_gray,
+      "background" => :black,
     },
   }
 end
--- src/epilog/reporters/fancy.cr
+++ src/epilog/reporters/fancy.cr
@@ -1,32 +1,17 @@
-require "crayon"
 require "stringpad"
+require "colorize"
 require "../log_types"
 
 module Epilog
-  WHITE = Crayon::Crayon.new("white", "black")
-
   class FancyReporter
     include Epilog::LogTypes
 
-    @crayon_instances = {} of String => Crayon::Crayon
-
-    def initialize
-      @options.each_key do |key|
-        emoji = @options[key]["emoji"].as(String)
-        foreground = @options[key]["foreground"].as(String)
-        background = @options[key]["background"].as(String)
-        @crayon_instances[key] = Crayon::Crayon.new(foreground, background, {
-          "bold" => true,
-        })
-      end
-    end
-
     def format(log_type, message)
-      colors = @crayon_instances.fetch(log_type)
-      emoji = @options[log_type]["emoji"].as(String)
-      colored_emoji = colors.apply(emoji)
-      colored_log_type = colors.apply(log_type)
-      colored_message = WHITE.apply(message)
+      foreground = @options[log_type]["foreground"].as(Symbol)
+      background = @options[log_type]["background"].as(Symbol)
+      colored_emoji = @options[log_type]["emoji"].colorize.fore(foreground).back(background).mode(:bold)
+      colored_log_type = log_type.colorize.fore(foreground).back(background).mode(:bold)
+      colored_message = message.colorize(:white)
       "#{colored_emoji} #{Stringpad.rpad(colored_log_type, " ", 8 - log_type.size)} #{colored_message}"
     end
   end
